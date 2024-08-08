9729f0e6e754fae0f3c3d7a9b5a5e43d1e63f839
igorspesh <kryaksp@gmail.com>
2024-08-08 21:43:48 +0400

    не работет скрол/тыкание && компакт мод

--- src/main/java/dev/cattyn/shulkerview/utils/ShulkerInfo.java
+++ src/main/java/dev/cattyn/shulkerview/utils/ShulkerInfo.java
@@ -2,6 +2,8 @@ package dev.cattyn.shulkerview.utils;
 
 import dev.cattyn.shulkerview.ShulkerViewEntrypoint;
 import net.minecraft.block.ShulkerBoxBlock;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.ContainerComponent;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -13,38 +15,26 @@ import java.awt.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public record ShulkerInfo(ItemStack shulker, boolean compact, int color, int slot, List<ItemStack> stacks) {
 
     public static ShulkerInfo create(ItemStack stack, int slot) {
         if (!(stack.getItem() instanceof BlockItem) || !(((BlockItem) stack.getItem()).getBlock() instanceof ShulkerBoxBlock)) return null;
         ShulkerBoxBlock block = (ShulkerBoxBlock) ((BlockItem) stack.getItem()).getBlock();
+
+        ContainerComponent blockContainer = stack.getComponents().get(DataComponentTypes.CONTAINER);
         List<ItemStack> items = DefaultedList.ofSize(27, ItemStack.EMPTY);
 
-        NbtCompound nbt = stack.getOrCreateNbt().contains("BlockEntityTag", 10)
-                ? stack.getNbt().getCompound("BlockEntityTag") : stack.getNbt();
+        IntStream.range(0, blockContainer.stream().toList().size()).forEach(i -> items.set(i, blockContainer.stream().toList().get(i)));
 
         boolean compact = ShulkerViewEntrypoint.getInstance().getConfig().isCompact();
-
-        if (nbt.contains("Items", 9)) {
-            Item unstackable = null;
-            NbtList nbt2 = nbt.getList("Items", 10);
-            for (int i = 0; i < nbt2.size(); i++) {
-                int slot2 = nbt2.getCompound(i).contains("Slot", 99) ? nbt2.getCompound(i).getByte("Slot") : i;
-                ItemStack item = ItemStack.fromNbt(nbt2.getCompound(i));
-                items.set(slot2, item);
-                if (item.getMaxCount() == 1) {
-                    if (unstackable != null && !item.getItem().equals(unstackable)) compact = false;
-                    unstackable = item.getItem();
-                }
-            }
-        }
 
         if (compact) {
             Map<Item, Integer> map = new HashMap<>();
             for (ItemStack item : items) {
                 if (item.isEmpty()) continue;
-
                 map.put(item.getItem(), item.getCount() + map.getOrDefault(item.getItem(), 0));
             }
             items.clear();
@@ -57,11 +47,19 @@ public record ShulkerInfo(ItemStack shulker, boolean compact, int color, int slo
 
         int color = 0xff9953b0;
         if (block.getColor() != null) {
-            float[] components = block.getColor().getColorComponents();
-            color = new Color(components[0], components[1], components[2]).hashCode();
+
+            int[] col = RGBIntToRGB(block.getColor().getSignColor());
+            color = new Color(col[0], col[1], col[2]).hashCode();
         }
 
         return new ShulkerInfo(stack, compact, color, slot, items);
+    }
+
+    public static int[] RGBIntToRGB(int in) {
+        int red = in >> 8 * 2 & 0xFF;
+        int green = in >> 8 & 0xFF;
+        int blue = in & 0xFF;
+        return new int[]{red, green, blue};
     }
 
 }
