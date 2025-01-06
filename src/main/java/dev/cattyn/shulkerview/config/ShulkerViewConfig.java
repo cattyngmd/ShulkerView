cef6bf82d5e39960518f30bc36fa1cf568fe564a
cattyn <cvenby@gmail.com>
2025-01-06 22:47:25 +0300

    added behaviour for both sides

--- src/main/java/dev/cattyn/shulkerview/config/ShulkerViewConfig.java
+++ src/main/java/dev/cattyn/shulkerview/config/ShulkerViewConfig.java
@@ -5,15 +5,19 @@ import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import me.shedaniel.math.Color;
 
-
 @Config(name = "shulker-view")
 public class ShulkerViewConfig implements ConfigData {
     @ConfigEntry.ColorPicker(allowAlpha = true)
     private int background = Color.ofRGBA(0, 0, 0, 75).hashCode();
     private boolean compact = true;
+    private boolean bothSides = true;
 
     public boolean isCompact() {
         return compact;
+    }
+
+    public boolean isBothSides() {
+        return bothSides;
     }
 
     public int getBackground() {
--- src/main/java/dev/cattyn/shulkerview/handler/RenderHandler.java
+++ src/main/java/dev/cattyn/shulkerview/handler/RenderHandler.java
@@ -7,50 +7,72 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.slot.SlotActionType;
 import net.minecraft.util.math.MathHelper;
+import org.joml.Math;
 import org.joml.Vector2d;
 
 public class RenderHandler implements Globals {
+    private static final int MARGIN = 2;
+    private static final int GRID_WIDTH = 20;
+    private static final int GRID_HEIGHT = 18;
+
     private final Vector2d clicked = new Vector2d();
     private int height, offset;
+    private int rows, cols;
+
+    private int startX;
+    private int currentY;
 
     public void render(DrawContext context, double mouseX, double mouseY) {
-        int y = 3 + offset;
+        boolean right = false;
+        startX = MARGIN;
+        currentY = usesBoth() ? MARGIN : MARGIN + offset;
+
         for (ShulkerInfo shulkerInfo : ShulkerViewEntrypoint.getInstance().getUpdateHandler().getShulkerList()) {
-            int count = 0, x = 2, startY = y, maxX = 22;
-            for (ItemStack stack : shulkerInfo.stacks()) {
-                if (shulkerInfo.compact() && stack.isEmpty()) break;
-                if (count > 0 && count % 9 == 0) {
-                    x = 2;
-                    y += 18;
-                }
-                context.drawItem(stack, x + 2, y);
-                if (stack.getCount() > 999) {
-                    String text = "%.1fk".formatted(stack.getCount() / 1000f);
-                    context.drawItemInSlot(mc.textRenderer, stack, x + 2, y, text);
-                } else {
-                    context.drawItemInSlot(mc.textRenderer, stack, x + 2, y);
-                }
-                x += 20;
-                count++;
-                if (x > maxX) maxX = x;
+            updateShulkerInfo(shulkerInfo);
+
+            if (currentY >= context.getScaledWindowHeight() && usesBoth() && !right) {
+                right = true;
+                currentY = MARGIN + offset;
             }
-            if (count == 0 && shulkerInfo.compact()) {
-                context.drawItem(shulkerInfo.shulker(), x + 2, y + 1);
+
+            if (right) {
+                startX = context.getScaledWindowWidth() - cols * GRID_WIDTH - MARGIN;
             }
-            y += 18;
-            if (clicked.lengthSquared() != 0
-                    && clicked.x >= 2 && clicked.x <= maxX
-                    && clicked.y >= startY && clicked.y <= y) {
-                mc.interactionManager.clickSlot(mc.player.currentScreenHandler.syncId, shulkerInfo.slot(), 0, SlotActionType.PICKUP, mc.player);
-                clicked.set(0);
-            }
-            context.fill(2, startY, maxX, y, ShulkerViewEntrypoint.getInstance().getConfig().getBackground());
-            context.fill(2, startY - 1, maxX, startY, shulkerInfo.color());
-            y += 2;
+            handleShulkerInfo(context, shulkerInfo);
         }
 
-        height = y - offset;
+        height = currentY - offset;
         clicked.set(0);
+    }
+
+    private void handleShulkerInfo(DrawContext context, ShulkerInfo info) {
+        int cols = this.cols * GRID_HEIGHT;
+        int rows = this.rows * GRID_WIDTH;
+        int width = cols + MARGIN * this.cols;
+
+        int x = startX;
+        int y = currentY;
+        int count = 0;
+        for (ItemStack stack : info.stacks()) {
+            if (info.compact() && stack.isEmpty()) break;
+            int column = x + (count % 9) * GRID_WIDTH + MARGIN;
+            int row = y + count / 9 * GRID_HEIGHT + MARGIN;
+            drawStack(context, stack, column, row);
+            count++;
+        }
+
+        if (count == 0 && info.compact()) {
+            context.drawItem(info.shulker(), x + MARGIN, currentY + MARGIN);
+        }
+        if (clicked.lengthSquared() != 0 && isHovered(clicked.x, clicked.y, width, rows)) {
+            int id = mc.player.currentScreenHandler.syncId;
+            mc.interactionManager.clickSlot(id, info.slot(), 0, SlotActionType.PICKUP, mc.player);
+            clicked.set(0);
+        }
+        int background = ShulkerViewEntrypoint.getInstance().getConfig().getBackground();
+        context.fill(x, y, x + width, y + rows, background);
+        context.fill(x, y - 1, width, y, info.color());
+        currentY += rows + MARGIN;
     }
 
     public void mouseClick(double x, double y, int button) {
@@ -61,5 +83,38 @@ public class RenderHandler implements Globals {
     public void mouseScroll(double x, double y, double amount) {
         if (amount == 0) return;
         this.offset = MathHelper.clamp((int) (offset + Math.ceil(amount) * 10), -height + mc.getWindow().getScaledHeight(), 0);
+    }
+
+    private boolean isHovered(double x, double y, float cols, float rows) {
+        return x >= startX && x <= startX + cols && y >= currentY && y <= currentY + rows;
+    }
+
+    private void drawStack(DrawContext ctx, ItemStack stack, int x, int y) {
+        ctx.drawItem(stack, x, y);
+        if (stack.getCount() > 999) {
+            String text = "%.1fk".formatted(stack.getCount() / 1000f);
+            ctx.drawItemInSlot(mc.textRenderer, stack, x, y, text);
+        } else {
+            ctx.drawItemInSlot(mc.textRenderer, stack, x, y);
+        }
+    }
+
+    private void updateShulkerInfo(ShulkerInfo info) {
+        int size = info.stacks().size();
+
+        if (info.compact()) {
+            size = 0;
+            for (ItemStack s : info.stacks()) {
+                if (s.isEmpty()) break;
+                size++;
+            }
+        }
+
+        rows = (int) Math.ceil(size / 9f);
+        cols = MathHelper.clamp(size, 1, 9);
+    }
+
+    private boolean usesBoth() {
+        return ShulkerViewEntrypoint.getInstance().getConfig().isBothSides();
     }
 }
